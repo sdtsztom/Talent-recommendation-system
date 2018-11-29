7cee191dc660239201038049a7800886573a702f
kannaduki <837070594@qq.com>
2018-11-30 00:13:42 +0800

    Activiti环境搭建

---
+++ tal_rec_sys/src/MVC/ActivitiService/serviceTask2.java
@@ -0,0 +1,15 @@
+package MVC.ActivitiService;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class serviceTask2 implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) {
+        String var = (String)delegateExecution.getVariable("var2");
+        System.out.println("传入serviceTask2的参数是" + var);
+    }
+}
