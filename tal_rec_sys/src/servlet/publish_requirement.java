8c8f9d5b5890765522d9522de8ef308fe5d8d4c5
a10442 <1044203335@qq.com>
2018-12-02 22:26:20 +0800

    bootstrap

--- tal_rec_sys/src/servlet/publish_requirement.java
+++ tal_rec_sys/src/servlet/publish_requirement.java
@@ -10,15 +10,18 @@ import java.io.IOException;
 @WebServlet(name = "publish_requirement")
 public class publish_requirement extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int rr_ri_id = Integer.parseInt(request.getAttribute("ri_id").toString());
+        int rr_ri_id = Integer.parseInt(request.getSession().getAttribute("ri_id").toString());
+        request.getSession().removeAttribute("ri_id");
         int rr_num = Integer.parseInt(request.getParameter("rr_num"));
         int rr_wp_id = Integer.parseInt(request.getParameter("rr_wp_id"));
         int rr_st_id = Integer.parseInt(request.getParameter("rr_st_id"));
         String rr_el = request.getParameter("rr_el").replace("T"," ");
         int rr_ept = Integer.parseInt(request.getParameter("rr_ept"));
         int rr_ed_id = Integer.parseInt(request.getParameter("rr_ed_id"));
-        String rr_spreq = request.getParameter("rr_spreq");
-
+        String rr_spreq = null;
+        if(request.getParameter("rr_spreq")!=null){
+            rr_spreq = request.getParameter("rr_spreq");
+        }
         
     }
 
