13db3f6fb7b92de71ebe1e1afdb8a4d79a653387
sdtsztom <sdtsztom@163.com>
2018-12-13 16:27:05 +0800

    Finish part of `Offer Confirm`
    - finsih pages and workflow code of `OfferConfirm` part

---
+++ tal_rec_sys/src/workflow/Tsk_offer_confirm.java
@@ -0,0 +1,29 @@
+package workflow;
+
+import bean.Arrangement;
+import ienum.*;
+import util.CommonConnection;
+import util.iutil;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Tsk_offer_confirm {
+    public static void record_res(String rpid){
+    }
+
+    public static void deal_points(String rpid){
+        String stf_id= CommonConnection.singleResultQuery("select rec_recstu_id from recommend where rec_rp_id="+rpid+" and rec_recsta_id="+RecStage.W_OC.toId(), ConnectUser.SYS);
+        CommonConnection.Update("insert into points_change values("+ PointsChangeRule.TW.toId()+
+                ","+stf_id+","+ iutil.getDate()+")",ConnectUser.SYS);
+    }
+
+    public static boolean finish(int rrid){
+        boolean unfinish_person= CommonConnection.existQuery("select * from recommend where rec_rr_id="+rrid+" and rec_recsta_id="+ RecStage.W_OC.toId(), ConnectUser.SYS);
+        if(unfinish_person)return false;
+        else{
+            CommonConnection.Update("update recruitment_requirements set rr_sta_id="+ RrStage.FINISH+" where rr_id"+rrid,ConnectUser.SYS);
+            return true;
+        }
+    }
+}
--- tal_rec_sys/web/SRMPages/Single_Rec_vOfferConfirm.jsp
+++ tal_rec_sys/web/SRMPages/Single_Rec_vOfferConfirm.jsp
@@ -1,4 +1,7 @@
-<%--
+<%@ page import="bean.LoginUser" %>
+<%@ page import="ienum.RrStage" %>
+<%@ page import="ienum.ConnectUser" %>
+<%@ page import="table.TableBase" %><%--
   Created by IntelliJ IDEA.
   User: sdtsz
   Date: 2018/11/27
@@ -9,10 +12,24 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
+<%  String rrid=request.getParameter("rrid");
+    LoginUser user=(LoginUser)session.getAttribute("user");
+    String name=user.getUsername();
+%>
 <head>
-    <title>Title</title>
+    <title>需求(rec_id:<%=rrid%>)的管理页面(状态：<%=RrStage.W_OC%>)</title>
 </head>
+<p>登录人：<%=name%></p>
+<p>需求号：<%=rrid%></p>
 <body>
+<form action="" method="post">
+    <%
+        TableBase table=new TableBase ("select rec_id,rec_rp_id,rec_rp_name,rec_stf_name,rec_from_desc,rec_recsta from SRM_OPEN where rec_rr_id="+rrid, ConnectUser.HR);
+        String []head={"推荐id","被推荐人id","被推荐人","推荐人","推荐来源","状态","详细信息"};
+        out.print(table.genHTML(head));
+    %>
 
+    <input type="submit" value="保存">
+</form>
 </body>
 </html>
