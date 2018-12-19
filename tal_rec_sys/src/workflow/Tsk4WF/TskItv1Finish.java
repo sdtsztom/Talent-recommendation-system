3ba6dbcb100d162f1455db511ba2e7656174a820
sdtsztom <sdtsztom@163.com>
2018-12-20 00:41:39 +0800

    修复各阶段的finish函数的错误使用

--- tal_rec_sys/src/workflow/Tsk4WF/TskItv1Finish.java
+++ tal_rec_sys/src/workflow/Tsk4WF/TskItv1Finish.java
@@ -1,10 +1,12 @@
 package workflow.Tsk4WF;
 
 import bean.Arrangement;
-import com.alibaba.fastjson.JSON;
+import ienum.ConnectUser;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
+import util.CommonConnection;
 import workflow.Tsk_Itv1;
+import workflow.Tsk_sift;
 
 public class TskItv1Finish implements JavaDelegate {
 
@@ -12,13 +14,8 @@ public class TskItv1Finish implements JavaDelegate {
     public void execute(DelegateExecution delegateExecution) {
         String json=(String)delegateExecution.getVariable("json");
         Arrangement[] arrangements=ArrangementListUnpacker.unpack2array(json);
-        boolean finish;
-        for(Arrangement arrangement:arrangements) {
-            if(Tsk_Itv1.finish(arrangement.getRec_id()) == false) {
-                finish = false;
-            }
-        }
-        finish = true;
+        String rrid= CommonConnection.singleResultQuery("select rec_rr_id from recommend where rec_id="+arrangements[0].getRec_id(), ConnectUser.SYS);
+        boolean finish = Tsk_Itv1.finish(rrid);
         delegateExecution.setVariable("isFinish",finish);
     }
 }
