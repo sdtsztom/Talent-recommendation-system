8d430ff9e4ca6e2d2769d80d67273361af89c6c1
sdtsztom <sdtsztom@163.com>
2018-12-20 09:52:14 +0800

    修复了RecStage没有同步数据库中记录的顺序更改的问题

--- tal_rec_sys/src/ienum/RecStage.java
+++ tal_rec_sys/src/ienum/RecStage.java
@@ -2,7 +2,7 @@ package ienum;
 
 // 对应的是recommend stage
 public enum RecStage {
-    FINISH,W_SIFT,W_I1,W_I2,W_OC,W_TW,W_ARR_S,W_ARR_I1;
+    FINISH,W_SIFT,W_ARR_S,W_I1,W_I2,W_OC,W_TW;
 
     @Override
     public String toString(){
@@ -10,11 +10,11 @@ public enum RecStage {
         switch(this){
             case FINISH:{stage="结束";break;}
             case W_SIFT:{stage="等待筛选";break;}
+            case W_ARR_S:{stage="等待安排(筛选后)";break;}
             case W_I1:{stage="等待初轮面试";break;}
             case W_I2:{stage="等待最终面试";break;}
             case W_OC:{stage="等待offer确认";break;}
             case W_TW:{stage="等待入职";break;}
-            case W_ARR_S:{stage="等待安排(筛选后)";break;}
         }
         return stage;
     }
@@ -24,11 +24,11 @@ public enum RecStage {
         switch(this){
             case FINISH:{id=1;break;}
             case W_SIFT:{id=2;break;}
-            case W_I1:{id=3;break;}
-            case W_I2:{id=4;break;}
-            case W_OC:{id=5;break;}
-            case W_TW:{id=6;break;}
-            case W_ARR_S:{id=7;break;}
+            case W_ARR_S:{id=3;break;}
+            case W_I1:{id=4;break;}
+            case W_I2:{id=5;break;}
+            case W_OC:{id=6;break;}
+            case W_TW:{id=7;break;}
         }
         return id;
     }
