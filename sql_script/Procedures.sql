72c6390ae37c8debe3009d8a2527da69c8a10322
sdtsztom <sdtsztom@163.com>
2018-12-20 19:41:33 +0800

    修复points_deal procedure中的bug

--- sql_script/Procedures.sql
+++ sql_script/Procedures.sql
@@ -39,7 +39,7 @@ BEGIN
 	DECLARE @rec_recstf_id char(40)
 	select @rec_recstf_id=rec_recstu_id from recommend where rec_id=@rec_id
 	insert into points_change values(@rule_id,@rec_recstf_id,GETDATE())
-	update stuff set stf_pts=stf_pts+(select ptchr_change from points_change_rule) where stf_id=@rec_recstf_id
+	update stuff set stf_pts=stf_pts+(select ptchr_change from points_change_rule where ptchr_id=@rule_id) where stf_id=@rec_recstf_id
 END
 GO
 
