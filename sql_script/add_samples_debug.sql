78a970b2f4d3ebdfa56102796de3312d8947cbe5
sdtsztom <sdtsztom@163.com>
2018-12-20 21:04:03 +0800

    添加debug用的sample script

--- sql_script/add_samples_debug.sql
+++ sql_script/add_samples_debug.sql
@@ -5,3 +5,8 @@ GO
 insert into interview values(1,1,1,2,2,1,'2018-12-19','2','无')
 insert into interview values(1,1,2,2,2,1,'2018-12-19','2','无')
 insert into interview values(1,1,3,2,2,1,'2018-12-19','2','无')
+
+-- for interview1
+insert into interview values(1,1,1,2,2,2,'2018-12-19','2','无')
+insert into interview values(1,1,2,2,2,2,'2018-12-19','2','无')
+insert into interview values(1,1,3,2,2,2,'2018-12-19','2','无')
