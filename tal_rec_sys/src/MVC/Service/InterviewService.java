3fdd6085c22955f71dc1b4dad72d9085fb862e90
kannaduki <837070594@qq.com>
2018-12-23 17:25:50 +0800

    interview build

--- tal_rec_sys/src/MVC/Service/InterviewService.java
+++ tal_rec_sys/src/MVC/Service/InterviewService.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public interface InterviewService {
 
-    int BuildInterview(String ip_id,String rp_id,String dealHR_id,String rr_id,String itv_time,String exmer_id,String itv_detail);
+    int BuildInterview(String ip_id,String rp_id,String dealHR_id,String rr_id,String itv_time,String exmer_id,String itv_detail,String itv_rnd);
 
     List<List<Map>> getSelect() throws Exception;
 
